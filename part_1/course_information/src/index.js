fe2713a0487711126ba8b3eafd8b6866b44f2fe5
Dax Wann <daxwann@gmail.com>
2019-10-25 18:50:30 -0700

    complete exercise 1.4 change parts to array of parts and refactor

--- part_1/course_information/src/index.js
+++ part_1/course_information/src/index.js
@@ -10,11 +10,13 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  const parts = props.parts.map((part) => {
+    return <Part part={part} />
+  });
+
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      {parts}
     </div>
   )
 }
@@ -28,37 +30,36 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.parts.reduce((total, part) => total + part.exercises, 0)
   return ( 
     <div>
-      <p>Number of exercises {props.count}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
