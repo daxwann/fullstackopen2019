ac249449148412bea2a26d33999d9656bed36072
Dax Wann <daxwann@gmail.com>
2019-10-25 18:35:18 -0700

    exercise 1.3: change parts variable and refactor

--- part_1/course_information/src/index.js
+++ part_1/course_information/src/index.js
@@ -22,7 +22,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <>
-      <p>{props.part.title} {props.part.exercises}</p>
+      <p>{props.part.name} {props.part.exercises}</p>
     </>
   )
 }
@@ -37,37 +37,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={
-          {
-            title: part1, 
-            exercises: exercises1
-          }
-        }
-        part2={
-          {
-            title: part2,
-            exercises: exercises2
-          }
-        }
-        part3={
-          {
-            title: part3,
-            exercises: exercises3
-          }
-        }
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
-      <Total count={exercises1 + exercises2 + exercises3} />
+      <Total count={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
