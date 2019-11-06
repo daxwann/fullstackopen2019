2ee9133fc7e1ccc9ee97f39fe43a806bb6769e5f
Dax Wann <daxwann@gmail.com>
2019-11-05 23:09:03 -0800

    complete 1.12

--- part_1/anecdotes/src/index.js
+++ part_1/anecdotes/src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = (props) => {
+	console.log("button");
+	return (
+  	<button onClick={props.handleClick}>next anecdote</button>
+	)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Display = (props) => {
+	return (
+  	<p>
+			{props.randomAnecdote}
+		</p>
+	)
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+	const setRandomSelection = (max, setSelected) => {
+		const rand = Math.floor(Math.random() * max);
+		setSelected(rand);
+	}
+
+  return (
+    <div>
+      <Display randomAnecdote={props.anecdotes[selected]} />
+			<Button handleClick={() => setRandomSelection(props.anecdotes.length, setSelected)}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
