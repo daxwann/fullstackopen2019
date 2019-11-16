928036fd641d193169a044969ef1786a2fd34970
Dax Wann <daxwann@gmail.com>
2019-11-15 16:09:32 -0800

    add display all names feature

---
+++ part_2/the_phonebook/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const showRow = () => {
+    return persons.map(person => <Person person={person}/>)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {showRow()}
+    </div>
+  )
+}
+
+export default App
